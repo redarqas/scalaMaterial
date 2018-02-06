249eb91252d9b6d0a76399a6153eea94cdd35f74
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2018-02-06 15:45:38 +0100

    upadte

---
+++ src/main/scala/team/is/learning/scala/jojax/Samples.scala
@@ -0,0 +1,15 @@
+package team.is.learning.scala.jojax
+
+object Samples {
+
+  def main(args: Array[String]): Unit = {
+    myMethode("jojax")
+
+  }
+
+  def myMethode(x: String): String = {
+    println(x)
+    s"coucou $x"
+  }
+
+}
