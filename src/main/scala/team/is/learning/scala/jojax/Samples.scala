fafe594435c3812fea7e6b2895bc513720268f65
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2018-02-06 16:34:15 +0100

    update

--- src/main/scala/team/is/learning/scala/jojax/Samples.scala
+++ src/main/scala/team/is/learning/scala/jojax/Samples.scala
@@ -3,7 +3,7 @@ package team.is.learning.scala.jojax
 object Samples {
 
   def main(args: Array[String]): Unit = {
-    myMethode("jojax")
+    inifinite3()
   }
 
   def myMethode(x: String): String = {
@@ -11,6 +11,19 @@ object Samples {
     s"coucou $x"
   }
 
-  def infinite() : Unit = ???
+  def infinite() : Unit = {
+    println("csdsdsd")
+    infinite()
+  }
+
+
+  def loop(n: Int) : Unit = {
+     println("coucou")
+     if(n == 2) () else loop(n + 1)
+  }
+
+  def inifinite3() : Unit = loop(0)
+
+
 
 }
